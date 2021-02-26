126edb0d1bdadeee7af23f6e6117a11beb64be52
RBioZ <jonatha_rihan@hotmail.com>
2021-02-25 23:41:02 -0300

    :sparkles: BugFix 1.1.1

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://lunion-mood.herokuapp.com/',
+  baseURL: 'https://iunion-mood.herokuapp.com/',
 });
 
 export default api;
